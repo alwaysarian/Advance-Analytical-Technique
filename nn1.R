36f58507d77435f6b775d0758c915a2bc24ca557
Praveen Kumar Neelappa <pnkumar@outlook.com>
2017-11-06 10:26:09 -0500

    v2

--- nn1.R
+++ nn1.R
@@ -1,19 +1,19 @@
 require(caret) #select tuning parameters
 require(e1071) #SVM
 
-eddat <- read.csv('EdgarMDPraveen.csv', as.is = T);
+eddat <- read.csv('EdgarMDPraveen1.csv', as.is = T);
 attach(eddat)
 
 #six group
-dm6 <- eddat[which(group == 'African American' | group == 'Asian American'| group == 'European America'| group == 'Native American'| group == 'CA Hispanic Amer'),];
+dm6 <- eddat[which(group == 'AfA' | group == 'AsA'| group == 'EA'| group == 'NAm'| group == 'HW'),];
 sapply(dm6, function(x) sum(!is.na(x))/length(x))
 colSums(is.na(eddat))
-table(na.omit(dm6[,c(9,12,13,16,17,19,20,22,23,24)])$group)#14,25
+table(na.omit(dm6[,c(9,12,13,16,17,19,20,22,23,24,14,25)])$group)#
 
-dm <- na.omit(dm6[,c(9,12,13,16,17,19,20,22,23,24)])
+dm <- na.omit(dm6[,c(9,12,13,16,17,19,20,22,23,24, 14,25)])
 dm$group <- as.factor(dm$group)
 
-dm <- cbind(dm["group"], scale(dm[,c(2:10)]) )
+dm <- cbind(dm["group"], scale(dm[,c(2:12)]) )
 
 
 
@@ -26,7 +26,7 @@ for (i in seq(100))
 {
   # sample
   inTrain <- createDataPartition(dm$group, p = .80, list = FALSE)
-  dm.navg <- avNNet(dm[c(2:10)], dm$group, subset = inTrain, size = 2, rang = 0.5, decay = 5e-4, maxit = 200, repeats = 50)
+  dm.navg <- avNNet(dm[c(2:12)], dm$group, subset = inTrain, size = 2, rang = 0.5, decay = 5e-4, maxit = 200, repeats = 50)
   Accuracies[i] <- confusionMatrix(dm$group[-inTrain], predict(dm.navg, dm[-inTrain,], type = "class"))$overall["Accuracy"]
 }
 # turn output on again
