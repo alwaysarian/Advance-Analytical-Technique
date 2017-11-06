b94920f3bcc5e5b3d822fa04d8a719d5008d5651
Praveen Kumar Neelappa <pnkumar@outlook.com>
2017-11-06 16:37:20 -0500

    12

--- nn2.R
+++ nn2.R
@@ -5,14 +5,14 @@ eddat <- read.csv('EdgarMDPraveen1.csv', as.is = T);
 attach(eddat)
 
 #six group
-dm6 <- eddat[which(group == 'AfA' | group == 'AsA'| group == 'EA'| group == 'NAm'| group == 'HW'),];
+dm6 <- eddat[which(group == 'AfA' | group == 'AsA'| group == 'EA'| group == 'NAm'| group == 'HW' | group == 'HSE'),];
 colSums(is.na(eddat))
-table(na.omit(dm6[,c(9,12,13,16,17,19,20,22,23,24,14,25)])$group)#
+table(na.omit(dm6[,c(9,12,13,16,17,19,20,22,23,24,14,25,15,18,21,29,30)])$group)#,11,15,18,21,29
 
-dm <- na.omit(dm6[,c(9,12,13,16,17,19,20,22,23,24,14,25)])
+dm <- na.omit(dm6[,c(9,12,13,16,17,19,20,22,23,24,14,25,15,18,21,29,30)])
 dm$group <- as.factor(dm$group)
 
-dm <- cbind(dm["group"], scale(dm[,c(2:12)]) )
+dm <- cbind(dm["group"], scale(dm[,c(2:13)]) )
 
 
 
@@ -25,7 +25,7 @@ for (i in seq(100))
 {
   # sample
   inTrain <- createDataPartition(dm$group, p = .80, list = FALSE)
-  dm.navg <- avNNet(dm[c(2:12)], dm$group, subset = inTrain, size = 2, rang = 0.5, decay = 5e-4, maxit = 200, repeats = 20)
+  dm.navg <- avNNet(dm[c(2:13)], dm$group, subset = inTrain, size = 2, rang = 0.5, decay = 5e-4, maxit = 200, repeats = 50)
   Accuracies[i] <- confusionMatrix(dm$group[-inTrain], predict(dm.navg, dm[-inTrain,], type = "class"))$overall["Accuracy"]
 }
 # turn output on again
